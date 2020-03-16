f9ccb873f5bf10be45e08a489aa6e398ae3514d1
A-1984 <ayoub@gmail.com>
2020-03-16 09:57:21 +0100

    I Success to made the first graphic programme hhhh in 3 days

--- tools/functions.h
+++ tools/functions.h
@@ -29,14 +29,8 @@
 
 	// +----> graphic_gtk/basic_functions.c
 
-	static void clear_surface (void);
-	static gboolean configure_event_cb (GtkWidget *widget,GdkEventConfigure *event, gpointer data);
-	static gboolean draw_cb (GtkWidget *widget, cairo_t *cr, gpointer data);
-	static void draw_brush (GtkWidget *widget, gdouble x, gdouble y);
-	static gboolean button_press_event_cb (GtkWidget *widget, GdkEventButton *event, gpointer data);
-	static gboolean motion_notify_event_cb (GtkWidget *widget, GdkEventMotion *event, gpointer data);
-	static void close_window (void);
-	static void activate (GtkApplication *app,gpointer user_data);
+	void draw();
+	static void activate (GtkApplication *app, gpointer user_data);
 	void initProg(int argc, char **argv);
 
 
